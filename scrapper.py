59f6e2d998e7107fd7c6448f770454462b0df7b6
AceVentura7 <100761095+AceVentura7@users.noreply.github.com>
2022-05-06 15:27:03 +0000

    added second csv

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium import wd # issue
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
@@ -139,4 +140,58 @@ df = scrape_nft()
 
 # saving the DataFrame as a CSV file
 nft_csv_data = df.to_csv('NFT.csv', index = False)
+print('\nCSV String:\n', nft_csv_data)
+
+
+
+# Function to get links from 2nd page for each of the NFT's. Due to time constraints using (.sleep) we shall only do 10.
+
+wd.get('https://www.flips.finance/') #issue
+
+
+def get_other_links():
+  final_list=[]
+  for i in range(10):
+    wd.get('https://www.flips.finance/')
+    time.sleep(3)
+    y=wd.find_elements(By.CLASS_NAME,'Rankings_tableRowLink__2wVPO')
+    #print(y[i])
+    x=y[i]
+    x.click()
+    time.sleep(5)
+    z=wd.find_elements(By.CLASS_NAME,'cstats_l__6AaiS')
+    #nft_links.append(z.get_attribute('href'))
+    nft_links = []
+    for j in z:
+      p=j.get_attribute('href')
+      nft_links.append(p)
+    final_list.append(nft_links)
+  return final_list
+
+other_links = get_other_links()
+
+#We are only going to scrape the top 10 NFTs on the page and get the links available on the same 10 pages to create another csv file.
+
+def scrape_nft2():
+    nft_url = 'https://www.flips.finance/'
+    driver=get_driver()
+    nft_dict2 = {
+        'Title': get_nft_title()[:10],
+        'Volume': get_nft_vol()[:10],
+        'Sales': get_nft_sales()[:10],
+        'Floor Price': nft_floor_price()[:10],
+        '24 hr Change': get_nft_change()[:10],
+        'Total Ownsers': get_nft_owners()[:10],
+        'Change Owners': get_nft_owners_change()[:10],
+        '% Supply Listed': nft_supply_listed()[:10],
+        '24 hr Listing Change': get_nft_listings()[:10],
+        'Other Links' : get_other_links()
+                
+    }
+    return pd.DataFrame(nft_dict2)
+
+df_2 = scrape_nft2()
+
+# saving the DataFrame as a CSV file
+nft_csv_data = df.to_csv('NFT(10).csv', index = False)
 print('\nCSV String:\n', nft_csv_data)
